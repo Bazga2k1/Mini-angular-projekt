96ee901937e2594e0d3b0d728e5940b47a2337fd
Bazga2k1 <filippo.bubic@gmail.com>
2024-02-06 17:11:08 +0100

    Rad na stranici za prijavu
    - formirana stranica za prijavu

--- mini-angular-projekt/src/app/login-component/login-component.component.ts
+++ mini-angular-projekt/src/app/login-component/login-component.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css'
 })
 
 export class LoginComponent {
   title = 'LoginComponent';
+  
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    console.log('Login clicked');
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
